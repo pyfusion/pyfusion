90bc997121d590ef95519f6f4a5acedbc27892db
david.pretty <david.pretty@7e179172-2c4c-0410-9cbb-abebdc96dd7f>
2008-10-03 11:28:31 +0000

    association rules using arules for R: no code here yet
    git-svn-id: https://pyfusion.googlecode.com/svn/trunk@282 7e179172-2c4c-0410-9cbb-abebdc96dd7f

---
+++ datamining/association_rules/utils.py
@@ -0,0 +1,14 @@
+"""
+"""
+
+def get_association_rules_for_clusterset(clusterset, channel_list):
+    """Return association rules with itemsets from channel list on the
+    left-hand side (LHS) and itemsets from the clusterset on the RHS.
+
+    clusterset is an instance of pyfusion.datamining.clustering.CusterSet
+    channel_list is a list of channel names whose channels have 
+    TimeSeriesDataSummary instances corresponding to the fluctuations
+    structures in the clusterset dataset.
+    
+    """
+    pass

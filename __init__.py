0afdf5cc33c92fa939a09b304b14a16c2c54b0b9
david.pretty <david.pretty@7e179172-2c4c-0410-9cbb-abebdc96dd7f>
2009-03-05 23:07:45 +0000

    add support for custom user config files in ~/.pyfusion
    git-svn-id: https://pyfusion.googlecode.com/svn/branches/refactor@347 7e179172-2c4c-0410-9cbb-abebdc96dd7f

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 import ConfigParser
- 
+import os
+
 from core import Device
 
 DEFAULT_CONFIG_FILE = 'pyfusion.cfg'
+USER_PYFUSION_DIR = os.path.join(os.path.expanduser('~'), '.pyfusion')
+if not os.path.exists(USER_PYFUSION_DIR):
+    os.mkdir(USER_PYFUSION_DIR)
+USER_CONFIG_FILE = os.path.join(USER_PYFUSION_DIR, 'pyfusion.cfg')
 
 config = ConfigParser.ConfigParser()
 config.read(DEFAULT_CONFIG_FILE)
+config.read(USER_CONFIG_FILE)
 
 # keep track of connected databases so we don't connect multiple
 # devices to the same database. There might be a better way to do this

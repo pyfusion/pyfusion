368bce22ae7615287f100845bf336e36e6530d5c
dave pretty <david.pretty@gmail.com>
2009-08-13 23:27:58 +1000

    started preparing for change in args, kwargs

--- acquisition/tests.py
+++ acquisition/tests.py
@@ -2,7 +2,19 @@
 
 from pyfusion.test.tests import BasePyfusionTestCase
 
+# Add new acquisition modules here for basic module structure test
 acquisition_modules = ['FakeData', 'MDSPlus']
+
+class TestAcquisitionArgs(BasePyfusionTestCase):
+    """Make sure we get the same result if we use config or kwargs"""
+
+    def testAcqArgs(self):
+        from pyfusion.acquisition.FakeData.acq import FakeDataAcquisition
+        acq_from_config = FakeDataAcquisition('test_fakedata')
+        # create a FakeDataAcquisition instance with keyword args
+        from pyfusion import config
+        config_option_list = config.pf_options('Acquisition', 'test_fakedata')
+        
 
 class TestGetAcquisition(BasePyfusionTestCase):
     """test getAcquisition function."""
@@ -13,6 +25,8 @@ class TestGetAcquisition(BasePyfusionTestCase):
         from pyfusion.acquisition.FakeData.acq import FakeDataAcquisition
         test_acq_2 = FakeDataAcquisition('test_fakedata')
         self.assertEqual(test_acq_1.__class__, test_acq_2.__class__)
+        self.assertEqual(test_acq_1.__dict__, test_acq_2.__dict__)
+
 
 class TestAcquisitionModules(BasePyfusionTestCase):
     """Check for existence of acquisition modules."""

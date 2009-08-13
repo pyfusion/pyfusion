d1461a946dc69f28e31a8b3adade08dbc38ca6b7
dave pretty <david.pretty@gmail.com>
2009-08-13 17:20:06 +1000

    start of mdsplus tests

--- acquisition/MDSPlus/tests.py
+++ acquisition/MDSPlus/tests.py
@@ -2,7 +2,6 @@
 
 from pyfusion.test.tests import BasePyfusionTestCase
 
-
 class TestMDSPlusDataAcquisition(BasePyfusionTestCase):
 
     def testBaseClasses(self):
@@ -12,8 +11,19 @@ class TestMDSPlusDataAcquisition(BasePyfusionTestCase):
 
 class TestMDSPlusDataFetchers(BasePyfusionTestCase):
 
-    def testDataFetchers(self):
+    def testDataFetcherBaseClass(self):
         from pyfusion.acquisition.base import BaseDataFetcher
         from pyfusion.acquisition.MDSPlus.fetch import MDSPlusDataFetcher
         self.assertTrue(BaseDataFetcher in MDSPlusDataFetcher.__bases__)
 
+class TestMDSPlusH1Connection(BasePyfusionTestCase):
+    """tests which require access to h1data.anu.edu.au"""
+
+    def testH1Data(self):
+        from pyfusion.acquisition.MDSPlus.acq import MDSPlusAcquisition
+        #h1mds = MDSPlusAcquisition(server="h1data.anu.edu.au")
+        #test_data = h1.acq.getdata(45637, "mirnov_array_1_coil_1")
+        
+TestMDSPlusH1Connection.h1 = True
+TestMDSPlusH1Connection.net = True
+TestMDSPlusH1Connection.slow = True

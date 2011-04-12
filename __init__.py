84806b1125ea49b9c006fbffba5540bba84a8e6b
Dave Pretty <david.pretty@gmail.com>
2011-04-12 23:23:22 +1000

    comment __init__.py, add version code from Fabric

--- __init__.py
+++ __init__.py
@@ -1,60 +1,58 @@
 import os, logging
+import logging.config
 
-#import pyfusion.conf
 from pyfusion.conf import PyfusionConfigParser
 from pyfusion.conf.utils import read_config
+from pyfusion.orm import ORMManager
+from pyfusion.version import get_version
 
+# This grabs the directory of the pyfusion module.
+PYFUSION_ROOT_DIR = os.path.dirname(__file__)
+
+# Location of the logging configuration file.
+LOGGING_CONFIG_FILE = os.path.join(PYFUSION_ROOT_DIR, 'conf', 'logging.cfg')
+
+# Grab the pyfusion version
+VERSION = get_version()
+
+# This creates a parser to process and store configuration file data.
+# PyfusionConfigParser is a subclass of ConfigParser.ConfigParser in the
+# python standard library. It is customised to parse [Type:Name] sections
+# in configuration files.
 config = PyfusionConfigParser()
 
-from pyfusion.orm import ORMManager
+# This allows us to activate and de-activate the object relational
+# mapping (ORM) code from within a python session, and also helps to
+# keep the ORM code separate so it doesn't slow things down for
+# users who don't want the database backend. 
 orm_manager = ORMManager()
 
-
-# set up logger
-import logging.config
-PYFUSION_ROOT_DIR = os.path.dirname(__file__)
-logging.config.fileConfig(os.path.join(PYFUSION_ROOT_DIR, "conf","logging.cfg"))
+# This sets up an instance of logger from the python standard library.
+logging.config.fileConfig(LOGGING_CONFIG_FILE)
 logger = logging.getLogger("pyfusion")
 
-
-
-# version information
-VERSION = (0, 0, 1, '', 0)
-
-def get_version():
-    """Human-readable version info."""
-    
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        version = '%s %s' % (version, VERSION[3])
-        if VERSION[3] != 'final':
-            version = '%s %s' % (version, VERSION[4])
-    """
-    # not yet implemented
-    from pyfusion.utils.version import get_git_revision
-    git_rev = get_git_revision()
-    if git_rev != u'Git-unknown':
-        version = "%s %s" % (version, git_rev)
-    """
-    return version
-
-
-# find config files
+# Find the default pyfusion configuration file...
 DEFAULT_CONFIG_FILE = os.path.join(PYFUSION_ROOT_DIR, 'pyfusion.cfg')
+
+# ... and the user's custom configuration file. First, if they don't
+# already have a folder for pyfusion stuff then let's make one
 USER_PYFUSION_DIR = os.path.join(os.path.expanduser('~'), '.pyfusion')
 if not os.path.exists(USER_PYFUSION_DIR):
     os.mkdir(USER_PYFUSION_DIR)
+# and here is the custom user configuration file
 USER_CONFIG_FILE = os.path.join(USER_PYFUSION_DIR, 'pyfusion.cfg')
-USER_TEST_CONFIG_FILE = os.path.join(USER_PYFUSION_DIR, 'tests.cfg')
 
+# Also allow specification of other configuration files from
+# a PYFUSION_CONFIG_FILE environment variable
 USER_ENV_CONFIG_FILE = os.getenv('PYFUSION_CONFIG_FILE','')
 
+# Now we actually loat the configuration files. Settings in
+# DEFAULT_CONFIG_FILE will be superseded by those in USER_CONFIG_FILE,
+# and USER_ENV_CONFIG_FILE will supersede both. As well as storing the
+# settings, read_config() will set up the ORM backend if required.
 read_config([DEFAULT_CONFIG_FILE, USER_CONFIG_FILE, USER_ENV_CONFIG_FILE])
 
+# We import these into the base pyfusion namespace for convenience.
 from pyfusion.devices.base import getDevice
 from pyfusion.acquisition.utils import getAcquisition
 
--- orm/__init__.py
+++ orm/__init__.py
@@ -1,32 +1,7 @@
 """Object relational mapping for Pyfusion"""
 
 import pyfusion
-"""
-if pyfusion.USE_ORM:
-    import sqlalchemy
 
-def setup_orm():
-    from sqlalchemy import create_engine, MetaData
-    from sqlalchemy.orm import scoped_session, sessionmaker
-
-    pyfusion.orm_engine = create_engine(pyfusion.config.get('global', 'database'))
-    pyfusion.Session = scoped_session(sessionmaker(autocommit=False,
-                                                   autoflush=True,
-                                                   bind=pyfusion.orm_engine,
-                                                   expire_on_commit=False))
-    if not hasattr(pyfusion, 'metadata'):
-        pyfusion.metadata = MetaData()
-    pyfusion.metadata.bind = pyfusion.orm_engine
-    pyfusion.metadata.create_all()
-
-
-def takedown_orm():
-    del pyfusion.metadata
-    del pyfusion.Session
-    del pyfusion.orm_engine
-
-
-"""
 class ORMManager(object):
     def __init__(self):
         self.func_list = []

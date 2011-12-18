dc7c0a3b5620d8a421fff2b13722aaf269d6b70c
David Pretty <david.pretty@anu.edu.au>
2011-12-18 16:42:39 +1100

    add orm for LHD

--- pyfusion/devices/LHD/device.py
+++ pyfusion/devices/LHD/device.py
@@ -1,4 +1,13 @@
 from pyfusion.devices.base import Device
+from pyfusion.orm.utils import orm_register
 
 class LHD(Device):
     pass
+
+@orm_register()
+def orm_load_lhddevice(man):
+    from sqlalchemy import Table, Column, Integer, ForeignKey
+    from sqlalchemy.orm import mapper
+    man.lhddevice_table = Table('lhddevice', man.metadata, 
+                            Column('basedevice_id', Integer, ForeignKey('devices.id'), primary_key=True))
+    mapper(LHD, man.lhddevice_table, inherits=Device, polymorphic_identity='lhd')
